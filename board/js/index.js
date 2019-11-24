895abad4773c4631d73a1d3ebffa760259ae1808
Краснов Кирилл Владимирович <krasnov.k.v@sberbank.ru>
2019-11-24 09:29:53 +0300

    без компонент

--- board/js/index.js
+++ board/js/index.js
@@ -3,44 +3,62 @@ let app = new Vue({
     data: {
         title: 'Test',
         text: 'Text Text Text',
-        // textRu: 'Текст Текст Текст',
+        date: 5,
+        month: 'Jun',
         reverseCard: false,
         cards: null,
-        cardsJSON: [
-            {
-                "theme": "Conversation",
-                "sourceText": "Do you speak a language other than English?",
-                "translation": "Говоришь ли ты на другом языке кроме английского?"
-            },
-            {
-                "theme": "Eating out",
-                "sourceText": "Excuse me. We would like to order, please.",
-                "translation": "Извините, мы бы хотели сделать заказ."
-            },
-            {
-                "theme": "Eating out",
-                "sourceText": "A table for two, please.",
-                "translation": "Столик на двоих, пожалуйста."
-            },
-        ],
+        err: '',
     },
 
     computed: {
+        calcDate() {
+            return this.date = new Date().getDate()
+        },
+        calcMonth() {
+            return this.month = new Date().toLocaleString('en', { month: 'short' })
+        },
+
     },
+
+    created: {},
 
     methods: {
-        translate() {
-            this.reverseCard = !this.reverseCard
-            setTimeout(() => {
-                this.reverseCard = false
-            }, 3000);
-            return
-        }
-    },
-    mounted() {
-        this.cards = this.cardsJSON
+        async getData() {
+            try {
+                this.cards = await fetch('https://raw.githubusercontent.com/sber-info/sber-info.github.io/master/board/json/Phrases.json')
+                    .then(data => data.json())
+            }
+            catch {
+                this.err = error
+            }
+        },
+        translate(card) {
+            return this.reverseCard = !this.reverseCard
+            // this.f(card, this.reverseCard )
+            // card.sourceText
+            // console.log(card.translation)
 
-        // this.cards = JSON.parse(this.cardsJSON)
-        // console.log(this.cards)
+            // this.reverseCard = !this.reverseCard
+            // setTimeout(() => {
+            //     this.reverseCard = false
+            // }, 3000);
+        },
+
+        backTr(card) {
+            return this.reverseCard ? card.sourceText : card.translation
+        },
+
+        sortByWords() {
+            // this.cards.sort((a, b) => {
+            //     a.sourceText.length > b.sourceText.length ? 1 : -1
+            // })
+            // this.cards.sort()
+            return this.cards
+        },
+    },
+
+    async mounted() {
+        await this.getData()
+        //сортировака 3х массивов
     }
 })  
