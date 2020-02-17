13e6e0361feb45da2c273b3b1a85fb5378131c14
Кирилл Краснов <kirill.v.krasnov@gmail.com>
2020-02-18 01:05:03 +0300

    pre done

--- bbcode_converter/js/index.js
+++ bbcode_converter/js/index.js
@@ -15,8 +15,18 @@ function convert() {
     visualBbcode.innerHTML = inputTable.value;
     //здесь замена синтаксиса
     outputBbcode.innerText = inputTable.value
-        .replace(/(<\w*)\s.*?(\>)/g, '$1$2')
-        .replace(/\</g, '[')
+
+        .replace(/(\<)img\s.*?src="(.*?)".*?(\>)/g, '$1image$3$2$1/image$3')
+        .replace(/(\<)a\s.*?class="button".*?\shref="(.*?)".*?(\>)(.*?)(\<\/)a\>/g, '$1urlb=$2$3$4$5urlb$3')
+        .replace(/(\<)a\s.*?href="(.*?)"\s.*?class="button".*?(\>)(.*?)(\<\/)a\>/g, '$1urlb=$2$3$4$5urlb$3')
+        .replace(/(\<)a\s.*?href="(.*?)".*?(\>)(.*?)(\<\/)a\>/g, '$1urln=$2$3$4$5urln$3')
+
+    .replace(/(\<\w*)\s.*?font-size: (..)px.*?(\>)(.*?)(\<\/\w*\>)/g, '$1$3<size=$2>$4</size>$5')
+        .replace(/(\<\w*)\s.*?color: (#.*?)[;"].*?(\>)(.*?)(\<\/\w*\>)/g, '$1$3<color=$2>$4</color>$5')
+        .replace(/(\<\w*)\s.*?text-align: (left|center|right)[;"].*?(\>)(.*?)(\<\/\w*\>)/g, '$1$3<$2>$4</$2>$5')
+        .replace(/(<\w*)\s.*?(class="text").*?(\>)/g, '$1 class="text"$3')
+
+    .replace(/\</g, '[')
         .replace(/\>/g, ']')
         .replace(/(\s)*/g, '$1')
         .replace(/(\])\s*(\[)/g, '$1$2')
@@ -24,5 +34,3 @@ function convert() {
 
 
 inputTable.addEventListener('keyup', convert);
-
-
