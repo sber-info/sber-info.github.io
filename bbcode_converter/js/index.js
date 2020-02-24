be68379a2411b35dfb163b8a6348ec70e3b78867
Кирилл Краснов <kirill.v.krasnov@gmail.com>
2020-02-24 23:33:12 +0300

    вложенность тегов

--- bbcode_converter/js/index.js
+++ bbcode_converter/js/index.js
@@ -15,7 +15,7 @@ function convert() {
     visualBbcode.innerHTML = inputTable.value;
     //здесь замена синтаксиса
     outputBbcode.innerText = inputTable.value
-
+        .replace(/\n*/g, '')
         .replace(/(\<)img\s.*?src="(.*?)".*?(\>)/g, '$1image$3$2$1/image$3')
         .replace(/(\<)a\s.*?class="button".*?\shref="(.*?)".*?(\>)(.*?)(\<\/)a\>/g, '$1urlb=$2$3$4$5urlb$3')
         .replace(/(\<)a\s.*?href="(.*?)"\s.*?class="button".*?(\>)(.*?)(\<\/)a\>/g, '$1urlb=$2$3$4$5urlb$3')
@@ -24,11 +24,22 @@ function convert() {
         .replace(/(\<)ul.*?(\>)(.*?)(\<\/)ul\>/g, '$1list$2$3$4list$2')
         .replace(/(\<)ol.*?(\>)(.*?)(\<\/)ol\>/g, '$1list=ol$2$3$4list=ol$2')
 
-    .replace(/(\<\w*)\s.*?font-size: (..)px.*?(\>)(.*?)(\<\/\w*\>)/g, '$1$3<size=$2>$4</size>$5')
-        .replace(/(\<\w*)\s.*?color: (#.*?)[;"].*?(\>)(.*?)(\<\/\w*\>)/g, '$1$3<color=$2>$4</color>$5')
-        .replace(/(\<\w*)\s.*?text-align: (left|center|right)[;"].*?(\>)\n(.*?)(\<\/\w*\>)/g, '$1$3<$2>$4</$2>$5')
-        // .replace(/(\<\w*)\s.*?[.*?(class="text")].*?text-align: (left|center|right)[;"].*?(\>)(.*?)(\<\/\w*\>)/g, '$1 class="text"$3<$2>$4</$2>$5')
-        // .replace(/(<\w*)\s.*?(class="text").*?(\>)/g, '$1 class="text"$3')
+    .replace(/(\<p)\s.*?font-size: (..)px[;"].*?(\>)\n*(\n*.*)\n*(\<\/p\>)/g, '$1$3<size=$2>$4</size>$5')
+        .replace(/(\<table)\s.*?font-size: (..)px[;"].*?(\>)\n*(.*)\n*(\<\/table\>)/g, '$1$3<size=$2>$4</size>$5')
+        .replace(/(\<td)\s.*?font-size: (..)px[;"].*?(\>)\n*(\n*.*)\n*(\<\/td\>)/g, '$1$3<size=$2>$4</size>$5')
+
+    .replace(/(\<p)\s.*?color: (#.*?)[;"].*?(\>)\n*(\n*.*)\n*(\<\/p\>)/g, '$1$3<color=$2>$4</color>$5')
+        .replace(/(\<table)\s.*?color: (#.*?)[;"].*?(\>)\n*(\n*.*)\n*(\<\/table\>)/g, '$1$3<color=$2>$4</color>$5')
+        .replace(/(\<td)\s.*?color: (#.*?)[;"].*?(\>)\n*(\n*.*)\n*(\<\/td\>)/g, '$1$3<color=$2>$4</color>$5')
+        .replace(/(\<a)\s.*?color: (#.*?)[;"].*?(\>)\n*(\n*.*)\n*(\<\/a\>)/g, '$1$3<color=$2>$4</color>$5')
+
+    .replace(/(\<td)\s.*?text-align: (left|center|right)[;"].*?(\>)\n*(\n*.*)\n*(\<\/td\>)/g, '$1$3<$2>$4</$2>$5')
+        .replace(/(\<table)\s.*?text-align: (left|center|right)[;"].*?(\>)\n*(\n*.*)\n*(\<\/table\>)/g, '$1$3<$2>$4</$2>$5')
+        .replace(/(\<p)\s.*?text-align: (left|center|right)[;"].*?(\>)\n*(\n*.*)\n*(\<\/p\>)/g, '$1$3<$2>$4</$2>$5')
+
+    // .replace(/(\<\w*)\s.*?text-align: (left|center|right)[;"].*?(\>)(.*?)(\<\/\w*\>)/g, '$1$3<$2>$4</$2>$5')
+    // .replace(/(\<\w*)\s.*?[.*?(class="text")].*?text-align: (left|center|right)[;"].*?(\>)(.*?)(\<\/\w*\>)/g, '$1 class="text"$3<$2>$4</$2>$5')
+    .replace(/(<\w*)\s.*?(class="text").*?(\>)/g, '$1 class="text"$3')
 
     .replace(/\</g, '[')
         .replace(/\>/g, ']')
