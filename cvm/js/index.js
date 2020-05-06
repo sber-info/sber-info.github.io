7bb75971a21e15b0261274d49c8743ef728c1628
Кирилл Краснов <kirill.v.krasnov@gmail.com>
2020-05-06 20:59:49 +0300

    tz 2

--- cvm/js/index.js
+++ cvm/js/index.js
@@ -3,7 +3,6 @@ const myInput = document.getElementById('myInput')
 const myDropdown = document.getElementById('myDropdown')
 const radio = document.getElementsByName('radio-button')
 
-
 function visibleAllList() {
   for (let i = 0; i < items.length; i++) {
     items[i].style.display = 'block';
@@ -11,16 +10,16 @@ function visibleAllList() {
 }
 
 function enabledDisabled() {
-  if (document.getElementById('disabled').checked==true) {
-    myInput.disabled='disabled';
+  if (document.getElementById('disabled').checked == true) {
+    myInput.disabled = 'disabled';
   }
-  if (document.getElementById('enabled').checked==true) {
-    myInput.disabled='';
+  if (document.getElementById('enabled').checked == true) {
+    myInput.disabled = '';
   }
 };
 
 function hideAllList(el) {
-  if ((el.target.id == !'myDropdown') && (document.getElementById('enabled').checked==true)) {
+  if ((el.target.id == !'myDropdown') && (document.getElementById('enabled').checked == true)) {
     for (let i = 0; i < items.length; i++) {
       items[i].style.display = 'none';
     }
@@ -30,31 +29,29 @@ function hideAllList(el) {
 function selectedItem(el) {
   if (el.target.className == 'dropdown_item') {
     myInput.value = document.getElementById(el.target.id).innerText;
-    // console.log(document.getElementById(el.target.id).innerText);
     for (let i = 0; i < items.length; i++) {
       items[i].style.display = 'none';
     }
   };
 };
 
-
 function filter() {
   let a = myDropdown.getElementsByTagName('a');
   for (let i = 0; i < a.length; i++) {
-      if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-        a[i].style.display = 'block';
-      } else {
-        a[i].style.display = 'none';
-      }
+    if (a[i].innerHTML.indexOf(myInput.value) > -1) {
+      console.log(a[i].innerHTML);
+      a[i].style.display = 'block'
     }
+    else {
+      a[i].style.display = 'none';
+    };
+  }
 }
-
-
 
 myInput.addEventListener('click', visibleAllList);
 document.addEventListener('click', (el) => hideAllList(el));
 document.addEventListener('click', (el) => selectedItem(el));
 document.addEventListener('click', enabledDisabled);
-myInput.addEventListener('keydown', filter);
+myInput.addEventListener('keyup', filter);
 
 
